bf6c78f2d59e66a33621f563a6a32203608b652d
James Alves <james@jamesalves.com.br>
2024-06-05 00:00:29 -0300

    update spec

--- back/src/core/users/user.spec.js
+++ back/src/core/users/user.spec.js
@@ -1,7 +1,13 @@
 import { create } from "./user.usecase.js";
 
 describe("Name of the group", () => {
-  test("should ", () => {
+  
+  test("should", () => {
     expect(create("james", "alves", "james@jamesalve.som.br").email).toBe("james@jamesalve.som.br");
   });
+
+  test("should 2", () => {
+    expect(create("james", "alves", "james@jamesalve.som.br").email).toBe("james@jamesalve.som.br");
+  });
+
 });

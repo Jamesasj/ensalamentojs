203ab24918e0ce909d303d21c01f9cc2fb76f2dd
James Alves <james@jamesalves.com.br>
2024-06-17 21:15:31 -0300

    add windsor to logger

--- back/src/ports/http/index.js
+++ back/src/ports/http/index.js
@@ -6,10 +6,12 @@ import swaggerFile from "../../../swagger.json" assert { type: "json" };
 import {db} from '../db/index.js'
 import { logger } from "../logs/config.js";
 import { adapter } from "../../adapters/index.js";
+import cors from "cors";
 const _app = express();
 
 const adp = adapter(db, logger)
 /* Middlewares */
+_app.use(cors());
 _app.use(bodyParser.json());
 _app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 

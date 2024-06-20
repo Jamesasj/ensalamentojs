b6209f684ae6af341ea0cd8ca425c4b5ddbe517b
James Alves <james@jamesalves.com.br>
2024-06-19 21:20:57 -0300

    test schedule confict

--- back/src/core/schedules/schedule.spec.js
+++ back/src/core/schedules/schedule.spec.js
@@ -5,6 +5,15 @@ describe('Name of the group', () => {
         const newSchedule = {challenge: {name : "projeto integrador"}}
         const schedules = [];
         const ret = uc.validateNew(newSchedule, schedules );
-        expect(ret).toBe(true);
+        expect(ret).toBe(newSchedule);
+    })
+
+
+
+    test('conflict schedule - same hour and day' , () => {
+        //const uc = ScheduleUseCase();
+        const newSchedule = {challenge: {name : "projeto integrador v", professor: {id:1, name:"james"} , start_hr:18, end_hr:22, dayOfWeak:4}}
+        const schedules = [{challenge: {name : "projeto integrador ii", professor: {id:1, name:"james"} , start_hr:18, end_hr:22, dayOfWeak:4}}];
+        expect(()=> uc.validateNew(newSchedule, schedules )).toThrow();
     })
 });
--- back/src/core/schedules/schedule.usecase.js
+++ back/src/core/schedules/schedule.usecase.js
@@ -2,8 +2,12 @@ export const ScheduleUseCase = {
     validateNew : (newSchedule, schedules ) => {
         
         if(schedules.length === 0){
-            return true
+            return newSchedule
         }
-        return false;
+
+        if(schedules.find((item)=> item.professor.id == newSchedule.professor.id &&  item.professor.dayOfWeak == newSchedule.dayOfWeak && item.professor.start_hr == newSchedule.professor.start_hr )){
+            throw new Error('0001: schedule conflict');
+        }
+        return newSchedule;
     }
 }
